bc2ae07ec99e0a00aa6f36b16c44709fd3ef1182
k-jogama <pg.kj50@gmail.com>
2020-08-05 14:31:46 +0900

    ログイン機能追加

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,46 @@
-@extends('layouts.app')
+@extends('app')
+
+@section('title', 'ログイン')
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+  <div class="row">
+    <div class="mx-auto col col-12 col-sm-11 col-md-9 col-lg-7 col-xl-6">
+      <h1 class="text-center"><a class="text-dark" href="/">drawroom</a></h1>
+      <div class="card mt-3">
+        <div class="card-body text-center">
+          <h2 class="h3 card-title text-center mt-2">{{ config('const.LOGIN') }}</h2>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+          @include('error_card_list')
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+          <div class="card-text">
+            <form method="POST" action="{{ route('login') }}">
+              @csrf
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+              <div class="md-form">
+                <label for="email">{{ config('const.MAILADDRESS') }}</label>
+                <input class="form-control" type="text" id="email" name="email" required value="{{ old('email') }}">
+              </div>
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+              <div class="md-form">
+                <label for="password">{{ config('const.PASSWORD') }}</label>
+                <input class="form-control" type="password" id="password" name="password" required>
+              </div>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+              <input type="hidden" name="remember" id="remember" value="on">
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+              <button class="btn btn-block blue-gradient mt-2 mb-2" type="submit">{{ config('const.LOGIN') }}</button>
 
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+            </form>
 
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
+            <div class="mt-0">
+              <a href="{{ route('register') }}" class="card-text">{{ config('const.USER_ENTRY_HERE') }}</a>
             </div>
+
+          </div>
         </div>
+      </div>
     </div>
+  </div>
 </div>
 @endsection
