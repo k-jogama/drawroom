a7d0d7f2102385b8f025caf12d1043c0ee73e041
k-jogama <pg.kj50@gmail.com>
2020-08-06 11:14:13 +0900

    投稿処理追加

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,18 +3,32 @@
 namespace App\Http\Controllers;
 
 use App\Post;
+use App\Http\Requests\PostRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
     public function index()
     {
         $posts = Post::all()->sortByDesc('created_at');
+        foreach ($posts as $key => $post) {
+            $username = DB::table('users')->where('id', $post['user_id'])->value('name');
+            $posts[$key]['username'] = $username;
+        }
         return view('index', ['posts' => $posts]);
     }
 
     public function create()
     {
         return view('posts.create');
+    }
+
+    public function store(PostRequest $request, Post $post)
+    {
+        $post->fill($request->all());
+        $post->user_id = $request->user()->id;
+        $post->save();
+        return redirect()->route('home.index');
     }
 }
--- app/Post.php
+++ app/Post.php
@@ -7,6 +7,13 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
+    protected $fillable = [
+        'title',
+        'body',
+        'worklocation',
+        'deliverydate',
+    ];
+
     public function user(): BelongsTo
     {
         return $this->belongsTo('App\User');
